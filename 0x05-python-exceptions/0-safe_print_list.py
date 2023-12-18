cc0fede18a68521012870dcde63727d77980b765
Giftbatolu <sekinatoyelami@gmail.com>
2023-12-18 04:53:02 -0800

    python

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    #printed_num = 0
+    printed_num = 0
     try:
-        printed_num = 0
+        #printed_num = 0
         for b in range(x):
             print(my_list[b], end="")
 
