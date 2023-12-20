3506ce009620528e51df19fc485d2d6a1760eb39
Giftbatolu <sekinatoyelami@gmail.com>
2023-12-20 14:50:02 -0800

    python

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    num_printed = 0
+    for b in range(x):
+        try:
+            print("{:d}".format(my_list[b]), end="")
+            num_printed += 1
+        except (ValueError, TypeError):
+            continue
+    print()
+    return num_printed
