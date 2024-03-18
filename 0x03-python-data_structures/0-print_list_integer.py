a440e8662819a467d0b7706fc9859a27eae2bd52
Giftbatolu <sekinatoyelami@gmail.com>
2024-03-18 09:01:54 -0700

    list

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+    """ Print all the integer in the list.
+        my_list:
+            The list to print.
+        Return: Nothing
+    """
+    for b in my_list:
+        print("{}".format(b))
