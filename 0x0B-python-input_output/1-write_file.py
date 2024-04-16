5cc5fa198659f02b4ad5b3540b2ad46635b87d80
Giftbatolu <sekinatoyelami@gmail.com>
2024-04-16 08:25:58 -0700

    Me

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+""" Defines a finction that write to a file. """
+
+
+def write_file(filename="", text=""):
+    """
+        function that write to a file.
+
+        Args:
+            filename - The file to write to.
+            text - The text the write.
+
+        Return: The written text.
+    """
+    with open(filename, "w", encoding="utf-8") as file_data:
+        return(file_data.write(text))
