af2f17365e399bc43bc1b4b0d103d54272df06b8
Giftbatolu <sekinatoyelami@gmail.com>
2024-05-07 23:46:22 -0700

    Square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+""" Defines a class Square """
+
+
+class Square:
+    """ A Square class """
+
+    def __init__(self, size):
+        """
+            class construtor
+
+            Args:
+                size (int) - The size of the square.
+
+        """
+
+        self.__size = size
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,1 @@
+This repository is about OOP - Object Oriented programming in python
