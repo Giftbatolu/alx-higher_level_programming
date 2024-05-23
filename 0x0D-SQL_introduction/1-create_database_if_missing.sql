63181690935d49b0dd11d5042c4731acd2ff096d
Giftbatolu <sekinatoyelami@gmail.com>
2024-05-23 10:36:34 -0700

    sql

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,2 @@
+-- Create database and shhould not raise error if already exist.
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,3 @@
+-- Prints the full description of the table first_table from the database hbtn_0c_0
+-- in MySQL server, and database name will be pass as argument.
+SHOW CREATE TABLE first_table;
