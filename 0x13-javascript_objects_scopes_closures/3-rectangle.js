bc0a694082de7d367d2fbd81d353dbac24c5889c
Giftbatolu <sekinatoyelami@gmail.com>
2024-06-21 15:58:01 -0700

    3-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (this.isvalid(w) && this.isvalid(h)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  isvalid (value) {
+    return (Number.isInteger(value) && value > 0);
+  }
+
+  print () {
+    let b, d, pattern;
+
+    for (b = 0; b < this.height; b++) {
+      pattern = '';
+      for (d = 0; d < this.width; d++) {
+        pattern += 'X';
+      }
+      console.log(pattern);
+    }
+  }
+};
