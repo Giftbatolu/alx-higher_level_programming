8b905a46d72bf80bac758d5fbc9433053b86b79d
Giftbatolu <sekinatoyelami@gmail.com>
2024-06-21 05:17:47 -0700

    Added some files

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -4,7 +4,7 @@ const { argv } = require('process');
 
 const num = parseInt(argv[2], 10);
 if (!isNaN(num)) {
-  console.log(`My number ${num}`);
+  console.log(`My number: ${num}`);
 } else {
   console.log('Not a number');
 }
